87c872bf961310e5cd5bf02998bf3224630bbc4a
Lucas S. Tavares <61010791+lstavares84@users.noreply.github.com>
2023-09-10 18:10:36 -0300

    Update ncdietpi.sh

--- ncdietpi.sh
+++ ncdietpi.sh
@@ -87,7 +87,7 @@ cd /docker/
 sudo mkdir nginx && cd nginx
 touch docker-compose.yml
 echo -e "Creating docker-compose.yml..."
-cat <<EOF >>/docker/nginx/docker-compose.yml
+sudo cat <<EOF >>/docker/nginx/docker-compose.yml
 version: '3.8'
 services:
   app:
@@ -142,13 +142,13 @@ echo -e "\033[1;32mCreated $CONF_FILE with the specified content.\033[0m"
 cat $CONF_FILE
 
 while true; do
-    read -p "Is the LOCAL IP address in $CONF_FILE correct for your Nextcloud? (Y/N): " user_input
+    read -p "[ ${BOLD_YELLOW}!${RESET_COLOR} ] Is the LOCAL IP address in $CONF_FILE correct for your Nextcloud? (Y/N): " user_input
     if [ "$user_input" == "Y" ] || [ "$user_input" == "y" ]; then
         break
     elif [ "$user_input" == "N" ] || [ "$user_input" == "n" ]; then
         nano $CONF_FILE
     else
-        echo -e "\033[1;31mInvalid input. Please enter 'Y' or 'N'.\033[0m"
+        echo -e "Invalid input. Please enter 'Y' or 'N'."
     fi
 done
 
@@ -180,59 +180,44 @@ echo -e "\033[1;32mConfig.php updated and maintenance:update:htaccess executed.\
 
 ################## END OF STEP 4 ###################
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ETAPA 9 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+###################### STEP 5 ######################
 # Prompt user to configure NGINX Proxy Manager
 while true; do
-    read -p "Please configure NGINX Proxy Manager now (E-mail Adsress: admin@example.com | Password: changeme) . Once done, type 'CONTINUE' to proceed with the script: " user_input
+    read -p "[ ${BOLD_YELLOW}!${RESET_COLOR} ] Please configure NGINX Proxy Manager now (E-mail Adsress: ${YELLOW}admin@example.com${RESET_COLOR} | Password: ${YELLOW}changeme${RESET_COLOR}) . Once done, type 'CONTINUE' to proceed with the script: " user_input
     if [ "$user_input" == "CONTINUE" ]; then
         break
     else
-        echo -e "\033[1;31mInvalid input. Please type 'CONTINUE' to proceed IF NGINX is configured.\033[0m"
+        echo -e "Invalid input. Please type 'CONTINUE' to proceed IF NGINX is configured."
     fi
 done
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FIM ETAPA 9 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+################## END OF STEP 5 ###################
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ETAPA 10 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-while true; do
-    read -p "Do you want to install an External HDD/SSD as Data Storage? (Y/N): " user_input
-    if [ "$user_input" == "Y" ]; then
-        read -p "Please plug in the External HDD/SSD into USB 3.0. All data on the drive will be lost as it will be formatted. Continue? (Y/N): " format_input
-        if [ "$format_input" == "Y" ]; then
-            # Execute the script for external storage (assuming it's in the root directory)
-            sudo /nextcloud-dietpi-external-storage.sh
-        fi
-        break
-    elif [ "$user_input" == "N" ]; then
-        break
-    else
-        echo -e "\033[1;31mInvalid input. Please type 'Y' for Yes or 'N' for No.\033[0m"
-    fi
-done
+###################### STEP 6 ######################
 
 while true; do
     read -p "Access /var/www/nextcloud/config/config.php to edit domain and IP in another SSH Terminal Screen. After that, type 'CONTINUE' to proceed with the script: " user_input
     if [ "$user_input" == "CONTINUE" ]; then
         break
     else
-        echo -e "\033[1;31mInvalid input. Please type 'CONTINUE' to proceed IF config.php is configured.\033[0m"
+        echo -e "Invalid input. Please type 'CONTINUE' to proceed IF config.php is configured."
     fi
 done
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FIM ETAPA 10 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+################## END OF STEP 6 ###################
 
-
+###################### STEP 7 ######################
 # Output TODO items with formatting
-echo -e "\n\n\033[1;30m[\033[0m\033[1;33m   \033[1;1mTODO   \033[0m\033[1;30m]\033[0m \033[0;37mChange the password of Nextclou Admin.\033[0m"
+echo -e "\n\n\[ ${BOLD_YELLOW}!${RESET_COLOR} ]Change the password of Nextcloud Admin."
 sudo -u www-data php /var/www/nextcloud/occ user:resetpassword admin
+################## END OF STEP 7 ###################
 
 
-echo -e "\n\n\033[1;33m[\033[0m\033[1;32m OK \033[0;33m\033[1;33m]\033[0m \033[0mINSTALLATION COMPLETED!"
-echo -e "\033[1;32m───────────────────────────────────────────────────────────────────────────────────────────────────────\033[0m"
-echo -e "\033[1;32mThank you for using this script!"
-echo -e "If you found it helpful, consider supporting the developer by buying a coffee using the link below:\033[0m"
-echo -e "\n\033[1;34m    \033[34mbuymeacoffee.com/lstavares84\033[0m"
+echo -e "\n\n\${BOLD_GREEN}INSTALLATION COMPLETED!${RESET_COLOR}"
+echo -e "${LIGHT_GREEN}───────────────────────────────────────────────────────────────────────────────────────────────────────${RESET_COLOR}"
+echo -e "Thank you for using this script!"
+echo -e "If you found it helpful, consider supporting the developer by buying a coffee using the link below:"
+echo -e "\n                        ${LIGHT_GREEN}buymeacoffee.com/lstavares84${RESET_COLOR}"
 echo ""
-echo -e "\033[1;32mYour contribution helps maintain this project and enables the creation of more useful features in the future.\033[0m"
-echo -e "\033[1;32mThank you for your support!\033[0m"
-echo -e "\033[1;32m───────────────────────────────────────────────────────────────────────────────────────────────────────\033[0m"
+echo -e "Your contribution helps maintain this project and enables the creation of more useful features in the future."
+echo -e "Thank you for your support!"
+echo -e "${LIGHT_GREEN}───────────────────────────────────────────────────────────────────────────────────────────────────────${RESET_COLOR}"
